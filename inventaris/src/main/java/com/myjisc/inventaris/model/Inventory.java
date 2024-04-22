1601ea55dd29578f394dd230575030a21d0ca92c
KingGedorah <naufal.zhafari@ui.ac.id>
2024-04-22 12:16:36 +0700

    initial commit inventaris

---
+++ inventaris/src/main/java/com/myjisc/inventaris/model/Inventory.java
@@ -0,0 +1,43 @@
+package com.myjisc.inventaris.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+
+    @Id
+    private UUID idItem = UUID.randomUUID();
+
+    @NotNull
+    @Column(name = "nama_item", nullable = false)
+    private String namaItem;
+
+    @NotNull
+    @Column(name = "quantity_item", nullable = false)
+    private Long quantityItem;
+
+    @Column(name = "quantity_borrowed")
+    private Long quantityBorrowed;
+
+    @Lob
+    @Basic(fetch =  FetchType.EAGER)
+    @Column(name = "image_item")
+    private Byte[] imageItem;
+
+    @ManyToMany(mappedBy = "requestedItems", fetch = FetchType.LAZY)
+    List<InventoryRequest> listRequest;
+
+}
