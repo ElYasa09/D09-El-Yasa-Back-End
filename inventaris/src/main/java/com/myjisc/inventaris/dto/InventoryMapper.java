a5e2781629098af9188a78e73ca4194f790dcbe6
KingGedorah <naufal.zhafari@ui.ac.id>
2024-04-26 20:12:14 +0700

    feat(inventaris): update and delete inventory done

--- inventaris/src/main/java/com/myjisc/inventaris/dto/InventoryMapper.java
+++ inventaris/src/main/java/com/myjisc/inventaris/dto/InventoryMapper.java
@@ -3,9 +3,12 @@ package com.myjisc.inventaris.dto;
 import org.mapstruct.Mapper;
 
 import com.myjisc.inventaris.dto.request.CreateInventoryRequestDTO;
+import com.myjisc.inventaris.dto.request.UpdateInventoryRequestDTO;
 import com.myjisc.inventaris.model.Inventory;
 
 @Mapper(componentModel = "spring")
 public interface InventoryMapper {
     Inventory createRestInventoryDTOToInventory(CreateInventoryRequestDTO createInventoryRequestDTO);
+
+    Inventory updateRestInventoryDTOToInventory(UpdateInventoryRequestDTO updateInventoryRequestDTO);
 }
--- inventaris/src/main/java/com/myjisc/inventaris/service/InventoryRestServiceImpl.java
+++ inventaris/src/main/java/com/myjisc/inventaris/service/InventoryRestServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.rmi.NoSuchObjectException;
+
 import com.myjisc.inventaris.model.Inventory;
 import com.myjisc.inventaris.repository.InventoryDb;
 
@@ -11,14 +13,15 @@ import jakarta.transaction.Transactional;
 
 import java.io.IOException;
 import java.util.UUID;
+import java.util.List;
 
 @Service
 @Transactional
-public class InventoryRestServiceImpl implements InventoryRestService{
+public class InventoryRestServiceImpl implements InventoryRestService {
 
     @Autowired
     private InventoryDb inventoryDb;
-    
+
     @Autowired
     private ImageUtilService imageUtilService;
 
@@ -31,7 +34,7 @@ public class InventoryRestServiceImpl implements InventoryRestService{
     }
 
     @Override
-    public byte[] setRestItemImage(Inventory inventory, MultipartFile image) throws IOException{
+    public byte[] setRestItemImage(Inventory inventory, MultipartFile image) throws IOException {
         byte[] imageItem = imageUtilService.compressImage(image.getBytes());
         return imageItem;
     }
@@ -40,4 +43,61 @@ public class InventoryRestServiceImpl implements InventoryRestService{
     public Inventory getItemByIdItem(UUID idItem) {
         return inventoryDb.findById(idItem).orElse(null);
     }
+
+    @Override
+    public List<Inventory> retrieveAllItem() {
+        return inventoryDb.findAll();
+    };
+
+    @Override
+    public void deleteItem(UUID idItem) {
+        var inventory = getItemByIdItem(idItem);
+        if (inventory != null) {
+            inventoryDb.delete(inventory);
+        }
+    };
+
+    @Override
+    public Inventory updateItem(Inventory inventoryFromDTO) throws NoSuchObjectException {
+        var item = getItemByIdItem(inventoryFromDTO.getIdItem());
+        if (item == null) {
+            throw new NoSuchObjectException("Item not found");
+        } else {
+            item.setNamaItem(inventoryFromDTO.getNamaItem());
+            item.setQuantityItem(inventoryFromDTO.getQuantityItem());
+            inventoryDb.save(item);
+            return item;
+        }
+    };
+
+    @Override
+    public Inventory updateItem(Inventory inventoryFromDTO, MultipartFile file) throws IOException {
+        var item = getItemByIdItem(inventoryFromDTO.getIdItem());
+        if (item == null) {
+            throw new NoSuchObjectException("Item not found");
+        } else {
+            item.setNamaItem(inventoryFromDTO.getNamaItem());
+            item.setQuantityItem(inventoryFromDTO.getQuantityItem());
+            item.setImageItem(setRestItemImage(inventoryFromDTO, file));
+            inventoryDb.save(item);
+            return item;
+        }
+    };
+
+    @Override
+    public byte[] getImageItem(UUID idItem) throws NoSuchObjectException {
+        var item = getItemByIdItem(idItem);
+
+        if (item != null) {
+            byte[] image = imageUtilService.decompressImage(item.getImageItem());
+
+            if (image == null) {
+                throw new NoSuchObjectException("Image Not Found");
+            }
+
+            return image;
+        } else {
+            throw new NoSuchObjectException("Berita Not Found");
+        }
+    };
 }
